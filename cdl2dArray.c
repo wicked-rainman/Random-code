5f05254cf9fb74e6ce813ae37608a745404aa8af
wicked-rainman <40294416+wicked-rainman@users.noreply.github.com>
2020-07-20 10:24:16 +0100

    Update and rename load2dArray.c to cdl2dArray.c

---
+++ cdl2dArray.c
@@ -0,0 +1,27 @@
+//-------------------------------------------------------------------------------------
+// Function cdl2dArray
+//
+//  Purpose: Given a comma delimited string, load each value into a 2D array.
+//  First parameter is the number of rows in the array
+//  Second parameter is the number of columns in each row
+//  Third parameter is the address of the array to be loaded
+//  Fourth parameter is the comma delimited string.
+//
+//  Given char ary[5][100];
+//  load_array(100,5,ary[0],"Temperature,Humidity,Pressure,WindSpeed,Rainfall");
+//  Each array[n] gets comma delimited value "Temperature","Humidity"... Etc
+//
+// NOTE: Depends on function strpos
+//
+//-----------------------------------------------------------------------------------
+void cdl2Array(int len, int fieldcount,char *ary ,char* fields) {
+int k,i;
+int startpos=0;
+        for(k=0;k<fieldcount-1;k++) {
+                i=strpos(",",fields+startpos);
+                memset(ary+(k*len),0,len);
+                strncpy(ary+(k*len),fields+startpos,i);
+                startpos+=(i+1);
+        }
+        strcpy(ary+(k*len),fields+startpos);
+}
