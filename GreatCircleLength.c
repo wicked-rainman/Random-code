b4db23d261d3b4ce8d724f5c3ea888963d081053
wicked-rainman <40294416+wicked-rainman@users.noreply.github.com>
2020-07-20 15:54:01 +0100

    Create GreatCircleLength.c

---
+++ GreatCircleLength.c
@@ -0,0 +1,20 @@
+//Calculate great circle length based on decimal lat lon pairs
+
+double GreatCircleLength(double lat1, double lon1, double lat2, double lon2) {
+  double theta, dist;
+    theta = lon1 - lon2;
+    dist = sin(deg2rad(lat1)) * sin(deg2rad(lat2)) + cos(deg2rad(lat1)) * cos(deg2rad(lat2)) * cos(deg2rad(theta));
+    dist = acos(dist);
+    dist = rad2deg(dist);
+    dist = dist * 60 * 1.1515;
+    return (dist);
+  }
+}
+
+double deg2rad(double deg) {
+  return (deg * pi / 180);
+}
+
+double rad2deg(double rad) {
+  return (rad * 180 / pi);
+}
