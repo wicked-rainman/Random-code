c513146963914a5724dafa464e98b9bdd8da294d
wicked-rainman <40294416+wicked-rainman@users.noreply.github.com>
2024-12-30 06:28:56 +0000

    Update cdl2dArray.c
    Didn't like previous version because of lack of error checking

--- cdl2dArray.c
+++ cdl2dArray.c
@@ -2,28 +2,36 @@
 // Function cdl2dArray
 //
 //  Purpose: Given a comma delimited string, load each value into a 2D array.
-//  First parameter is the number of rows in the array
-//  Second parameter is the number of columns in each row
+//  First parameter is the length of the maximum field length in the array
+//  Second parameter is the number of fields in the array
 //  Third parameter is the address of the array to be loaded
-//  Fourth parameter is the comma delimited string.
+//  Fourth parameter is the comma delimited input string.
 //
 //  Given char ary[5][100];
 //  cdl2Array(100,5,ary[0],"Temperature,Humidity,Pressure,WindSpeed,Rainfall");
 //  Each array[n] gets the comma delimited values out of fields 
 //  for "Temperature","Humidity"... Etc
 //
-// NOTES: Depends on function strpos
-//        prototype for this function is: void cdl2dArray(int, int, char*, char*);
+// NOTES: prototype for this function is: void cdl2dArray(int, int, char*, char*);
 //
 //-----------------------------------------------------------------------------------
-void cdl2Array(int len, int fieldcount,char *ary ,char* fields) {
-int k,i;
-int startpos=0;
-        for(k=0;k<fieldcount-1;k++) {
-                i=strpos(",",fields+startpos);
-                memset(ary+(k*len),0,len);
-                strncpy(ary+(k*len),fields+startpos,i);
-                startpos+=(i+1);
+int cdl2Array(int FieldLen, int FieldCount,char *WordArray ,char* InputLine) {
+    int InputLineLength=0;
+    int k;
+    int ArrayPos=0;
+    int WordCount=0;
+    memset(WordArray,0,FieldCount*FieldLen);
+    InputLineLength=strlen(InputLine);
+    for(k=0;k<InputLineLength;k++) {
+        if(WordCount>=FieldCount) return -1;
+        if(InputLine[k]==',') {
+            WordCount++;
+            ArrayPos=0;
         }
-        strcpy(ary+(k*len),fields+startpos);
+        else {
+            if(ArrayPos>=FieldLen) return -1;
+            strncpy(WordArray+((WordCount*FieldLen)+ArrayPos++),InputLine+k,1);
+        }
+    }
+    return WordCount+1;
 }
